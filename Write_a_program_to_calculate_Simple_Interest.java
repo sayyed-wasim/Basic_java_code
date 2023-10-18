9a2056ed4209c990383c26e48113ff9808a7122d
Sayyed Wasim <82093277+sayyed-wasim@users.noreply.github.com>
2023-10-18 11:38:21 +0530

    Add files via upload
    calculate_Simple_Interest

---
+++ Write_a_program_to_calculate_Simple_Interest.java
@@ -0,0 +1,25 @@
+package ClearCode;
+
+public class Write_a_program_to_calculate_Simple_Interest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float Principle;
+		float rate;
+		float time;
+		float SimpleInterset;
+		
+		Principle=1500; rate=12; time=2;
+		
+		SimpleInterset=(Principle*rate*time)/100;
+		
+		System.out.println("Simple Interst"+SimpleInterset);
+		
+		
+		
+		
+
+	}
+
+}
